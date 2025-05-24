37096a2848cbb3ad16c2e4e3e5b96aaf65e2a828
MYoussef11 <marawanyoussef11@gmail.com>
2025-05-24 14:39:28 +0300

    update main, req., init.py, base.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env") # Load environment variables from .env file
+
+from routes import base
+
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {
-        "message": "Welcome to the FastAPI application!"
-        }
+app.include_router(base.base_router)
+
